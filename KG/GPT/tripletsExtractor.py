1e8049a8b4d16fdf9fbbb13ad6653de0e99bc160
Pampaj <leonardo.pamaploni666@gmail.com>
2024-02-27 11:36:25 +0100

    defined okay models

--- KG/GPT/tripletsExtractor.py
+++ KG/GPT/tripletsExtractor.py
@@ -1,6 +1,6 @@
 import wikipedia
 from openai import OpenAI
-import re
+import os
 
 
 def get_wikipedia_text():
@@ -27,53 +27,55 @@ def get_wikipedia_text():
 
 
 def get_AI_response(text=get_wikipedia_text()):
+    output_directory = "tripletsTXTs"
     client = OpenAI(
         api_key="LL-5OPT3xLPTWxSff1zUH42hQxtOBQPTLhqOlnHCMLrDYpRGIwgXaum7Rj8LWu31eXV",
         base_url="https://api.llama-api.com"
     )
 
     models = [
-        "llama-13b-chat",
-        # "llama-7b-32k", "llama-13b-chat", "llama-70b-chat", "mixtral-8x7b-instruct",
-        # "mistral-7b-instruct", "mistral-7b", "NousResearch/Nous-Hermes-Llama2-13b", "falcon-7b-instruct",
-        # "falcon-40b-instruct", "alpaca-7b", "codellama-7b-instruct", "codellama-34b-instruct", "vicuna-7b",
-        # "vicuna-13b",
+        "llama-13b-chat",  # ok
+        # "llama-7b-32k", #trash
+        "llama-13b-chat",  # god
+        "llama-70b-chat",  # ok
+        "mixtral-8x7b-instruct",  # ok
+        "mistral-7b-instruct",  # ok
+        # "mistral-7b", #trash
+        # "NousResearch/Nous-Hermes-Llama2-13b", #trash
+        # not working "falcon-7b-instruct",
+        # not working "falcon-40b-instruct",
+        # "alpaca-7b", #trash
+        # "codellama-7b-instruct", #trash
+        # "codellama-34b-instruct", #trash
+        "vicuna-7b",  # ok dai
+        "vicuna-13b",  # top
     ]
 
-    responses = []
-    for i in models:
-        print("Waiting for:", i)
+    for model in models:
+        print("Waiting for:", model)
         response = client.chat.completions.create(
-            model=i,
+            model=model,
             temperature=0,
             seed=42,
-            messages=[
-                {"role": "user",
-                 "content": "You will perform the open information extraction task. You will identify the named "
-                            "entities in the content and then extract the relations between them."
-                            "Based on the provided testimony, you will return triples which is formated as <named "
-                            "entity A,"
-                            "relation, named entity B>." + text + "The extracted triples formated as <named entity A, "
-                                                                  "relation, named entity B> are:"
-                 }
-
-            ]
-
+            messages=[{
+                "role": "user",
+                "content": ("You will perform the open information extraction task. You will identify the named "
+                            "entities in the content and then extract the relations between them. "
+                            "Based on the provided testimony, you will return triples which is formatted as <named "
+                            "entity A, relation, named entity B>." + text + " The extracted triples formatted as <named entity A, "
+                                                                            "relation, named entity B> are:")
+            }]
         )
-        responses.append(response.choices[0].message.content)
 
-    return responses, models
+        # Define the filename based on the model name
+        filename = f'extracted_text_from_{model.replace("/", "_")}.txt'
+
+        filepath = os.path.join(output_directory, filename)
+        # Open the file in write mode and write the triples
+        with open(filepath, 'w') as file:
+            file.write(response.choices[0].message.content)
+        print(f"Output saved to {filepath}")
 
 
 text_from_wiki = get_wikipedia_text()
-text_from_AI = get_AI_response(text_from_wiki)
-
-# Define the filename
-text_from_AItxt = 'extracted_text_from_llama.txt'
-# Open the file in write mode and write the triples
-
-
-with open(text_from_AItxt, 'w') as file:
-    file.write(text_from_AI[0][0])
-
-
+get_AI_response(text_from_wiki)
