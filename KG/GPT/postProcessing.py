3bd2a20ba4c0de13001c8f4e24ccfc19a1cbbc10
leonardo <leonardo.pampaloni29@gmail.com>
2024-03-10 01:32:50 +0100

    found something that can be for us -- cosine similarity beetween words with best fit

--- KG/GPT/postProcessing.py
+++ KG/GPT/postProcessing.py
@@ -1,10 +1,38 @@
-# import these modules
-from nltk.stem import WordNetLemmatizer
+import nltk
+from nltk.corpus import wordnet as wn
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
-lemmatizer = WordNetLemmatizer()
 
-print("rocks :", lemmatizer.lemmatize("rocks"))
-print("trees  :", lemmatizer.lemmatize("trees"))  # is this a joke?
+def get_synonyms(word):
+    synonyms = set()
+    for syn in wn.synsets(word):
+        for lemma in syn.lemmas():
+            synonyms.add(lemma.name())
+    return synonyms
 
-# a denotes adjective in "pos"
-print("better :", lemmatizer.lemmatize("better", pos="a"))
+
+word = "car"
+synonyms = get_synonyms(word)
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+
+def find_closest_embeddings(query_embedding, embeddings, labels, top_n=5):
+    similarities = cosine_similarity([query_embedding], embeddings)[0]
+    top_indices = np.argsort(similarities)[-top_n:]
+    return [(labels[i], similarities[i]) for i in top_indices[::-1]]
+
+
+syn_list = []
+for syn in synonyms:
+    syn_list.append(syn)
+
+labels = syn_list
+embeddings = np.array([model.encode(label) for label in labels])
+query_embedding = model.encode(word)  # basically is the cosine similarity between the synonyms of car and the word car
+# can be used in the calculate_matrix function in confusion_matrix.py to calculate the similarity between the predicted
+# and the correct triplets
+
+closest_synonyms = find_closest_embeddings(query_embedding, embeddings, labels)
+print(closest_synonyms)
